6ea1d9ca87b24ccefcf71efeb2f477a1f02fdb35
visergra <visergra@hotmail.com>
2017-05-18 18:48:18 +0200

    layout ok

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
-var game = new Game2048();
-game._renderBoard();
+window.onload = function () {
+  game = new Game2048();
+  renderTiles();
+};
+
+function renderTiles () {
+  game.board.forEach(function(row, rowIndex){
+    row.forEach(function (cell, cellIndex) {
+      if (cell) {
+        var tileContainer = document.getElementById("tile-container");
+        var newTile       = document.createElement("div");
+
+        newTile.classList  = "tile val-" + cell;
+        newTile.classList += " tile-position-" + rowIndex + "-" + cellIndex;
+        newTile.innerHTML  = (cell);
+
+        tileContainer.appendChild(newTile);
+      }
+    });
+  });
+}
